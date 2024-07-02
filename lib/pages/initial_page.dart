51dfef0e209cceed5cc1f6e36dae98fb1ecba6ad
Louay Barraq <louaybarraq@gmail.com>
2024-07-02 15:29:58 +0100

    Added a picture

--- lib/pages/initial_page.dart
+++ lib/pages/initial_page.dart
@@ -16,97 +16,98 @@ class _InitialPageState extends State<InitialPage> {
     return Scaffold(
       backgroundColor: Color.fromRGBO(121, 201, 158, 1.0),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: Column(
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Welcome To\nSmartDaigoku",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 40,
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    "Improving Your Experience Is Our\nResponsability",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 20,
-                    ),
-                  ),
-                  SizedBox(height: 40)
-                ],
-              ),
-              Center(
-                child: Column(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Column(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    ChoiceBox(mainText: "Student"),
-                    SizedBox(height: 25),
-                    ChoiceBox(mainText: "Professor"),
-                    SizedBox(height: 25),
-                    ChoiceBox(mainText: "Academic Staff Member"),
-                    SizedBox(height: 35),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20.0),
-                        color: Colors.black,
+                    Text(
+                      "Welcome To\nSmartDaigoku",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 40,
                       ),
-                      height: 52,
-                      width: 260,
-                      child: Center(
-                        child: Text(
-                          "Sign Up",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      "Improving Your Experience Is Our\nResponsibility",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                  ],
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      ChoiceBox(mainText: "Student"),
+                      SizedBox(height: 25),
+                      ChoiceBox(mainText: "Professor"),
+                      SizedBox(height: 25),
+                      ChoiceBox(mainText: "Academic Staff Member"),
+                      SizedBox(height: 35),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.black,
+                        ),
+                        height: 52,
+                        width: 260,
+                        child: Center(
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 5.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Already have an account ? ",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 17,
-                              fontWeight: FontWeight.w600,
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Already have an account ? ",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 17,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
-                          ),
-                          Text(
-                            "Login",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 17,
-                              fontWeight: FontWeight.w700,
+                            Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 17,
+                                fontWeight: FontWeight.w700,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    )
-                  ],
+                      Container(
+                        child: Image.asset(
+                          "assets/images/new_beginnings.png",
+                          width: 180,
+                          height: 180,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 200),
-                child: Image.asset(
-                  "assets/images/new_beginnings.png",
-                  width: 200,
-                  height: 200,
-                ),
-              )
-            ],
+              ],
+            ),
           ),
         ),
       ),
