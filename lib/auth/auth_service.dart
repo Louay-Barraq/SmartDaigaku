6eeb3d8aaffbabbe9e7020d75ee5c4a1c6bef152
Louay Barraq <louaybarraq@gmail.com>
2024-07-04 16:56:58 +0100

    Added the toasts and updated the auth functions

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -1,27 +1,66 @@
 import 'dart:ffi';
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class AuthService {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  Future<void> loginWithEmail(String email, String password) async {
+  Future<bool> loginWithEmail(
+      {required String email, required String password}) async {
     try {
       var userCredential = await _firebaseAuth.signInWithEmailAndPassword(
           email: email, password: password);
+
+      // Display success toast
+      Fluttertoast.showToast(
+        msg: 'Login successful !',
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.SNACKBAR,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        fontSize: 17.0,
+      );
+
+      return true;
     } on FirebaseAuthException catch (e) {
       String message = '';
-      if (e.code == 'user-not-found') {
-        message = 'No user found for that email.';
-      } else if (e.code == 'wrong-password') {
-        message = 'Wrong password provided for that user.';
+      switch (e.code) {
+        case 'invalid-email':
+          message = 'The email address is not valid.';
+          break;
+        case 'user-disabled':
+          message = 'The user account has been disabled.';
+          break;
+        case 'user-not-found':
+          message = 'No user found for that email.';
+          break;
+        case 'wrong-password':
+          message = 'Wrong password provided.';
+          break;
+        case 'too-many-requests':
+          message = 'Too many requests. Try again later.';
+          break;
+        case 'network-request-failed':
+          message = 'Network error. Please try again later.';
+          break;
+        default:
+          message = e.code;
       }
+      Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.SNACKBAR,
+        backgroundColor: Colors.black54,
+        textColor: Colors.white,
+        fontSize: 17.0,
+      );
+      return false;
     }
   }
 
-  Future<void> signUpWithEmail(
+  Future<bool> signUpWithEmail(
       {required String email,
       required String password,
       required String name}) async {
@@ -29,13 +68,48 @@ class AuthService {
       var userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
           email: email, password: password);
       await userCredential.user?.updateDisplayName(name);
+
+      // Display success toast
+      Fluttertoast.showToast(
+        msg: 'Sign-up successful!',
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.SNACKBAR,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        fontSize: 17.0,
+      );
+
+      return true;
     } on FirebaseAuthException catch (e) {
       String message = '';
-      if (e.code == 'weak-password') {
-        message = 'The provided password is too weak.';
-      } else if (e.code == 'email-already-in-use') {
-        message = 'An account already exists with the provided email.';
+      switch (e.code) {
+        case 'email-already-in-use':
+          message = 'The email address is already in use by another account.';
+          break;
+        case 'invalid-email':
+          message = 'The email address is not valid.';
+          break;
+        case 'operation-not-allowed':
+          message = 'Email/password accounts are not enabled.';
+          break;
+        case 'weak-password':
+          message = 'The password is too weak.';
+          break;
+        case 'network-request-failed':
+          message = 'Network error. Please try again later.';
+          break;
+        default:
+          message = e.code;
       }
+      Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.SNACKBAR,
+        backgroundColor: Colors.redAccent,
+        textColor: Colors.white,
+        fontSize: 17.0,
+      );
+      return false;
     }
   }
 }
