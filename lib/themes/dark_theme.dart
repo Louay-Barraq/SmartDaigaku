bf52cfac963dbd21edf3fd2a3a6cca7b7c2f3885
Louay Barraq <louaybarraq@gmail.com>
2024-07-08 11:11:01 +0100

    Added two new themes.

---
+++ lib/themes/dark_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.dark(
+    surface: Color(0xFF00513E),
+    primary: Color(0xFFFFFFFF),
+    inversePrimary: Color(0xFF000000),
+    secondary: Color(0xFF66D1A9),
+    onSecondary: Color(0xFF1A1A1A),
+    tertiary: Color(0xFFFFFFFF),
+    onTertiary: Color(0xFF000000),
+  ),
+  textTheme: ThemeData.dark().textTheme.apply(
+        bodyColor: Colors.grey[300],
+        displayColor: Colors.white,
+      ),
+  cardTheme: CardTheme(
+    surfaceTintColor: Color(0xFF1A1A1A),
+    color: Color(0xFFFFFFFF),
+  ),
+  useMaterial3: true,
+);
