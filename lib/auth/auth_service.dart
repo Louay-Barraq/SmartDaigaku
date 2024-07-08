7fa33013219a7c3834d546126fa190d2863d00e3
Louay Barraq <louaybarraq@gmail.com>
2024-07-08 11:10:01 +0100

    Added the google sign in methods.

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -1,15 +1,15 @@
 // ignore_for_file: unused_local_variable, unused_import
 
-import 'dart:ffi';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  Future<bool> loginWithEmail(
+  Future<UserCredential?> loginWithEmail(
       {required String email, required String password}) async {
     try {
       var userCredential = await _firebaseAuth.signInWithEmailAndPassword(
@@ -25,7 +25,7 @@ class AuthService {
         fontSize: 17.0,
       );
 
-      return true;
+      return userCredential;
     } on FirebaseAuthException catch (e) {
       String message = '';
       switch (e.code) {
@@ -41,6 +41,9 @@ class AuthService {
         case 'wrong-password':
           message = 'Wrong password provided.';
           break;
+        case 'invalid-credential':
+          message = 'The supplied auth credential is malformed or has expired.';
+          break;
         case 'too-many-requests':
           message = 'Too many requests. Try again later.';
           break;
@@ -54,15 +57,15 @@ class AuthService {
         msg: message,
         toastLength: Toast.LENGTH_LONG,
         gravity: ToastGravity.SNACKBAR,
-        backgroundColor: Colors.black54,
+        backgroundColor: Colors.red,
         textColor: Colors.white,
         fontSize: 17.0,
       );
-      return false;
+      return null;
     }
   }
 
-  Future<bool> signUpWithEmail(
+  Future<UserCredential?> signUpWithEmail(
       {required String email,
       required String password,
       required String name}) async {
@@ -70,6 +73,7 @@ class AuthService {
       var userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
           email: email, password: password);
       await userCredential.user?.updateDisplayName(name);
+      // await createUserDocument(userCredential, name);
 
       // Display success toast
       Fluttertoast.showToast(
@@ -81,7 +85,7 @@ class AuthService {
         fontSize: 17.0,
       );
 
-      return true;
+      return userCredential;
     } on FirebaseAuthException catch (e) {
       String message = '';
       switch (e.code) {
@@ -111,7 +115,126 @@ class AuthService {
         textColor: Colors.white,
         fontSize: 17.0,
       );
-      return false;
+      return null;
+    }
+  }
+
+  Future<UserCredential?> signInWithGoogle() async {
+    try {
+      final GoogleSignIn googleSignIn = GoogleSignIn();
+      // Sign out from Google to ensure user selection dialog appears
+      await googleSignIn.signOut();
+      final GoogleSignInAccount? googleSignInAccount =
+          await googleSignIn.signIn();
+      if (googleSignInAccount == null) {
+        // The user canceled the sign-in
+        Fluttertoast.showToast(
+          msg: 'Google sign-in canceled.',
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.SNACKBAR,
+          backgroundColor: Colors.amber,
+          textColor: Colors.white,
+          fontSize: 15.0,
+        );
+        return null;
+      }
+
+      final GoogleSignInAuthentication googleAuth =
+          await googleSignInAccount.authentication;
+      final OAuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      // Sign in to Firebase with the Google [UserCredential]
+      UserCredential userCredential =
+          await _firebaseAuth.signInWithCredential(credential);
+      // await createUserDocument(userCredential, userCredential.additionalUserInfo!.username!);
+
+      // Display success toast
+      Fluttertoast.showToast(
+        msg: 'Google sign-in successful !',
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.SNACKBAR,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        fontSize: 15.0,
+      );
+
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      String message = '';
+      switch (e.code) {
+        case 'account-exists-with-different-credential':
+          message =
+              'An account already exists with the same email address but different sign-in credentials.';
+          break;
+        case 'invalid-credential':
+          message = 'The supplied auth credential is malformed or has expired.';
+          break;
+        case 'operation-not-allowed':
+          message = 'Google sign-in is not enabled.';
+          break;
+        case 'user-disabled':
+          message = 'The user account has been disabled by an administrator.';
+          break;
+        case 'user-not-found':
+          message = 'No user found for that email.';
+          break;
+        case 'wrong-password':
+          message = 'Wrong password provided.';
+          break;
+        case 'invalid-verification-code':
+          message =
+              'The SMS verification code used to create the phone auth credential is invalid.';
+          break;
+        case 'invalid-verification-id':
+          message =
+              'The verification ID used to create the phone auth credential is invalid.';
+          break;
+        case 'network-request-failed':
+          message = 'Network error. Please try again later.';
+          break;
+        case 'too-many-requests':
+          message = 'Too many requests. Try again later.';
+          break;
+        default:
+          message = e.code;
+      }
+      Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.SNACKBAR,
+        backgroundColor: Colors.redAccent,
+        textColor: Colors.white,
+        fontSize: 15.0,
+      );
+      return null;
+    } catch (e) {
+      print("Caught Error: $e");
+      Fluttertoast.showToast(
+        msg: 'An unexpected error occurred. Please try again.',
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.SNACKBAR,
+        backgroundColor: Colors.redAccent,
+        textColor: Colors.white,
+        fontSize: 15.0,
+      );
+      return null;
+    }
+  }
+
+  Future<void> createUserDocument(
+      UserCredential? userCredential, String username) async {
+    if (userCredential != null && userCredential.user != null) {
+      await FirebaseFirestore.instance
+          .collection("Users")
+          .doc(userCredential.user!.email)
+          .set({
+        'email': userCredential.user!.email,
+        'username': username,
+        'timestamp': Timestamp.now()
+      });
     }
   }
 }
