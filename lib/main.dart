5fb9e92463d6b2d0dc89410ae260c30b09914ed4
oussema ben abdelkader <oussemabenabdelkader0@gmail.com>
2024-07-12 11:50:36 +0100

    Main Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,7 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_import, duplicate_ignore, use_key_in_widget_constructors
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:smart_daigoku/auth/auth_page.dart';
-import 'package:smart_daigoku/pages/home_page.dart';
-import 'package:smart_daigoku/pages/initial_page.dart';
-import 'package:smart_daigoku/pages/login_page.dart';
-import 'package:smart_daigoku/pages/signup_page.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:smart_daigoku/themes/dark_theme.dart';
-import 'package:smart_daigoku/themes/light_theme.dart';
-import 'package:smart_daigoku/themes/purple_dark_theme.dart';
-import 'package:smart_daigoku/themes/purple_light_theme.dart';
+import 'package:smart_daigoku/auth/auth_page.dart';
 import 'package:smart_daigoku/themes/theme_provider.dart';
 import 'firebase_options.dart';
 
@@ -31,10 +21,14 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: Provider.of<ThemeProvider>(context).themeData,
-      debugShowCheckedModeBanner: false,
-      home: AuthPage(),
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return MaterialApp(
+          theme: themeProvider.themeData,
+          debugShowCheckedModeBanner: false,
+          home: AuthPage(),
+        );
+      },
     );
   }
 }
